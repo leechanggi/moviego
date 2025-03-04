371b1ea78ba747797336642b3156b8d0c761a0bb
이창기 <67743720+leechanggi@users.noreply.github.com>
2025-03-04 15:22:25 +0900

    init

--- app/(router)/page.tsx
+++ app/(router)/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div></div>;
-}
+import React from 'react';
+import { Button } from '@/app/components';
+
+const Page = () => {
+  return (
+    <section>
+      <Button>버튼</Button>
+    </section>
+  );
+};
+
+export default Page;
